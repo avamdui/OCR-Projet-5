2f7ca5f5e969f69455e61518eff83a202077386e
avamdui <54531433+avamdui@users.noreply.github.com>
2021-11-30 11:01:47 +0100

    Template Fix

--- admin/templates/home/topbar.html.php
+++ admin/templates/home/topbar.html.php
@@ -1,15 +1,15 @@
 
-<nav class="navbar navbar-expand-lg text-white py-3">
+<nav class="navbar navbar-expand-lg navbar-dark text-white">
     <div class="container-fluid "
             <a href="#" class="text-white brand-logo"><img src="../img/logo.png" height="80" alt="" ></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mob-navbar" aria-label="Toggle">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                <ul class="nav justify-content-end">
-                    <li class="btn me-2"><a class="nav-link  text-white" href="index.php">Tableau de bord</a></li>
-                    <li class="btn me-2"><a class="nav-link text-white" href="index.php?controller=ArticleController&task=showAllArticlesWithPagination">Gestion des articles</a></li>
-                    <li class="btn me-2"><a class="nav-link text-white" href="index.php?controller=ArticleController&task=addpost">Ajouter un article</a></li>
+            <div class="collapse navbar-collapse justify-content-end" id="mob-navbar">
+                <ul class="nav nav-pills nav-fill">
+                    <li class="nav-item me-2"><a class="nav-link  text-white" href="index.php">Tableau de bord</a></li>
+                    <li class="nav-item me-2"><a class="nav-link text-white" href="index.php?controller=ArticleController&task=showAllArticlesWithPagination">Gestion des articles</a></li>
+                    <li class="nav-item me-2"><a class="nav-link text-white" href="index.php?controller=ArticleController&task=addpost">Ajouter un article</a></li>
                     <?php if (!empty($_SESSION)){
                     echo '<li class="btn me-2"><a class="nav-link  text-white"" href="index.php?controller=UserController&task=logout"">Se déconnecter ( ' . $_SESSION['first_name'] . ') </a></li>';
                     }

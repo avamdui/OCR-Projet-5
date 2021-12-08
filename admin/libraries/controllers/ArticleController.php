50b08f9b91ac5747d8d6ee27d96b554a7d2e2bd1
avamdui <54531433+avamdui@users.noreply.github.com>
2021-12-08 10:52:42 +0100

    Correction Bugs

--- admin/libraries/controllers/ArticleController.php
+++ admin/libraries/controllers/ArticleController.php
@@ -9,157 +9,134 @@ require_once('libraries/renderer.php');
 
 class ArticleController
 {
-    protected $msg = [];
-    public function showOneArticle()
-    {
-        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
-        $article = (new BlogService())->getOneArticleWithComments($id);
+        protected $msg = [];
+        public function showOneArticle()
+        {
+                $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+                $article = (new BlogService())->getOneArticleWithComments($id);
 
-        $avm = new ArticleViewModel();
-        $avm->article = $article;
-        $avm->pageTitle = $article->getTitle() . ' by ' . $article->getAuthor()->getFullname();
+                $avm = new ArticleViewModel();
+                $avm->article = $article;
+                $avm->pageTitle = $article->getTitle() . ' by ' . $article->getAuthor()->getFullname();
 
-        \Renderer::render('articles/show', compact('avm'));
-    }
-
-    public function showAllArticlesWithPagination()
-    {
-        $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
-        if (isset($page) && !empty($page)) {
-            $currentPage = (int) strip_tags($page);
-        } else {
-            $currentPage = 1;
-        }
-        $articleParPage = 5;
-
-
-        $service = new BlogService();
-        $articles = $service->getAllArticlesWithPagination($articleParPage, $currentPage);
-
-        $asvm = new ArticlesViewModel();
-        $asvm->articles = $articles;
-        $asvm->pageTitle = 'Nos articles - page ' . $currentPage;
-        $asvm->currentPage = $currentPage;
-        $asvm->totalPage = $service->getNombreTotalDePages($articleParPage);
-<<<<<<< Updated upstream
-        if (isset($_GET['msg']) && !empty($_GET['msg'])) {
-            $msg['success'] = '<div class="alert alert-success" role="alert"><h4>Publication réussite</h4></div>';
-            $asvm->msg;
-        }
-=======
-        $result = filter_input(INPUT_GET, 'msg', FILTER_SANITIZE_SPECIAL_CHARS);
-        if ($result === 'succeed') {
-            $msg['success'] = '<div class="alert alert-success" role="alert"><h4>Publication réussite</h4></div>';
-            $asvm->msg = $msg;
-        }
-        if ($result === 'fail') {
-            $msg['fail'] = '<div class="alert alert-danger " role="alert"><h4>ERREUR</h4></div>';
-            $asvm->msg = $msg;
+                \Renderer::render('articles/show', compact('avm'));
         }
 
-
->>>>>>> Stashed changes
-
-        \Renderer::render('articles/blog', compact('asvm'));
-    }
-
-    public function deleteArticle()
-    {
-        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
-
-        $article = new ModificationService();
-        // Je créer l'objet Article avec les commentaires. 
-
-        $id = $article->deleteArticle($id); // Je veut lancer la suppression de l'article
-        $article->deleteArticleComments($id); //Puis la suppressions des commentaires
-
-        //suppression de l'image
-        $extension = '.jpg';
-        $path = '.././img/posts/' . $id . $extension;
-        unlink($path);
-
-        \Http::redirect("index.php?controller=ArticleController&task=showAllArticlesWithPagination&page=1");
-    }
-
-    public function addpost()
-    {
-
-        $pageTitle = "Ajouter un article";
-        \Renderer::render("articles/addpost", compact('pageTitle'));
-    }
-
-    public function insertArticleandImage()
-    {
-        // Controller les infos
-        // ViewModel avec les erreurs et avec les valeurs deja saisies
-
-        // Enregistrement de l'article
-        $model = new ArticleCreationModel();
-<<<<<<< Updated upstream
-        $model->setAuthorId(1);
-=======
-        $model->setAuthorId($_SESSION['idUsers']);
->>>>>>> Stashed changes
-        $model->setChapo(filter_input(INPUT_POST, "chapo", FILTER_SANITIZE_SPECIAL_CHARS));
-        $model->setContent(filter_input(INPUT_POST, "content", FILTER_SANITIZE_SPECIAL_CHARS));
-        $model->setTitle(filter_input(INPUT_POST, "title", FILTER_SANITIZE_SPECIAL_CHARS));
-
-        $service = new ModificationService();
-        $nouvelId = $service->insertArticle($model);
-
-        // Enregistrement de l'image
-
-        $extension = '.jpg';
-        $path = '.././img/posts/' . $nouvelId . $extension;
-        $tmpName = $_FILES['image']['tmp_name'];
-        move_uploaded_file($tmpName, $path);
-
-        // Redirection vers le blog
-<<<<<<< Updated upstream
-        \Http::redirect("index.php?controller=articleController&task=showOneArticle&id=" . $nouvelId);
-=======
-        \Http::redirect("index.php?controller=articleController&task=showAllArticlesWithPagination&msg=succeed");
->>>>>>> Stashed changes
-    }
+        public function showAllArticlesWithPagination()
+        {
+                $page = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
+                if (isset($page) && !empty($page)) {
+                        $currentPage = (int) strip_tags($page);
+                } else {
+                        $currentPage = 1;
+                }
+                $articleParPage = 5;
 
 
-    public function editArticle()
-    {
+                $service = new BlogService();
+                $articles = $service->getAllArticlesWithPagination($articleParPage, $currentPage);
 
-        // Enregistrement de l'article
-        $model = new ArticleModificationModel();
-<<<<<<< Updated upstream
-        $model->setId(filter_input(INPUT_POST, "articleId", FILTER_SANITIZE_SPECIAL_CHARS));
-=======
-        $model->setId(filter_input(INPUT_POST, "articleId", FILTER_VALIDATE_INT));
->>>>>>> Stashed changes
-        $model->setChapo(filter_input(INPUT_POST, "chapo", FILTER_SANITIZE_SPECIAL_CHARS));
-        $model->setContent(filter_input(INPUT_POST, "content", FILTER_SANITIZE_SPECIAL_CHARS));
-        $model->setTitle(filter_input(INPUT_POST, "title", FILTER_SANITIZE_SPECIAL_CHARS));
-
-        $service = new ModificationService();
-        $service->editArticle($model);
-<<<<<<< Updated upstream
-        $id = filter_input(INPUT_GET, "articleId", FILTER_SANITIZE_SPECIAL_CHARS);
-=======
-        $id = filter_input(INPUT_POST, "articleId", FILTER_VALIDATE_INT);
->>>>>>> Stashed changes
+                $asvm = new ArticlesViewModel();
+                $asvm->articles = $articles;
+                $asvm->pageTitle = 'Nos articles - page ' . $currentPage;
+                $asvm->currentPage = $currentPage;
+                $asvm->totalPage = $service->getNombreTotalDePages($articleParPage);
+                $result = filter_input(INPUT_GET, 'msg', FILTER_SANITIZE_SPECIAL_CHARS);
+                if ($result === 'succeed') {
+                        $msg['success'] = '<div class="alert alert-success" role="alert"><h4>Publication réussite</h4></div>';
+                        $asvm->msg = $msg;
+                }
+                if ($result === 'fail') {
+                        $msg['fail'] = '<div class="alert alert-danger " role="alert"><h4>ERREUR</h4></div>';
+                        $asvm->msg = $msg;
+                }
 
 
-        // Suppression ancienne image
-        $extension = '.jpg';
-        $path = '.././img/posts/' . $id . $extension;
-        unlink($path);
 
-        // Enregistrement de l'image
+                \Renderer::render('articles/blog', compact('asvm'));
+        }
 
-        $extension = '.jpg';
-        $path = '.././img/posts/' . $id . $extension;
-        $tmpName = $_FILES['image']['tmp_name'];
-        move_uploaded_file($tmpName, $path);
+        public function deleteArticle()
+        {
+                $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
-        // Redirection vers le blog
+                $article = new ModificationService();
+                // Je créer l'objet Article avec les commentaires. 
 
-        \Http::redirect("index.php?controller=articleController&task=showAllArticlesWithPagination&msg=succeed");
-    }
+                $id = $article->deleteArticle($id); // Je veut lancer la suppression de l'article
+                $article->deleteArticleComments($id); //Puis la suppressions des commentaires
+
+                //suppression de l'image
+                $extension = '.jpg';
+                $path = '.././img/posts/' . $id . $extension;
+                unlink($path);
+
+                \Http::redirect("index.php?controller=ArticleController&task=showAllArticlesWithPagination&page=1");
+        }
+
+        public function addpost()
+        {
+
+                $pageTitle = "Ajouter un article";
+                \Renderer::render("articles/addpost", compact('pageTitle'));
+        }
+
+        public function insertArticleandImage()
+        {
+                // Controller les infos
+                // ViewModel avec les erreurs et avec les valeurs deja saisies
+
+                // Enregistrement de l'article
+                $model = new ArticleCreationModel();
+                $model->setAuthorId($_SESSION['idUsers']);
+                $model->setChapo(filter_input(INPUT_POST, "chapo", FILTER_SANITIZE_SPECIAL_CHARS));
+                $model->setContent(filter_input(INPUT_POST, "content", FILTER_SANITIZE_SPECIAL_CHARS));
+                $model->setTitle(filter_input(INPUT_POST, "title", FILTER_SANITIZE_SPECIAL_CHARS));
+
+                $service = new ModificationService();
+                $nouvelId = $service->insertArticle($model);
+
+                // Enregistrement de l'image
+
+                $extension = '.jpg';
+                $path = '.././img/posts/' . $nouvelId . $extension;
+                $tmpName = $_FILES['image']['tmp_name'];
+                move_uploaded_file($tmpName, $path);
+
+                // Redirection vers le blog
+                \Http::redirect("index.php?controller=articleController&task=showAllArticlesWithPagination&msg=succeed");
+        }
+
+
+        public function editArticle()
+        {
+
+                // Enregistrement de l'article
+                $model = new ArticleModificationModel();
+                $model->setId(filter_input(INPUT_POST, "articleId", FILTER_VALIDATE_INT));
+                $model->setChapo(filter_input(INPUT_POST, "chapo", FILTER_SANITIZE_SPECIAL_CHARS));
+                $model->setContent(filter_input(INPUT_POST, "content", FILTER_SANITIZE_SPECIAL_CHARS));
+                $model->setTitle(filter_input(INPUT_POST, "title", FILTER_SANITIZE_SPECIAL_CHARS));
+
+                $service = new ModificationService();
+                $service->editArticle($model);
+                $id = filter_input(INPUT_POST, "articleId", FILTER_VALIDATE_INT);
+
+
+                // Suppression ancienne image
+                $extension = '.jpg';
+                $path = '.././img/posts/' . $id . $extension;
+                unlink($path);
+
+                // Enregistrement de l'image
+
+                $extension = '.jpg';
+                $path = '.././img/posts/' . $id . $extension;
+                $tmpName = $_FILES['image']['tmp_name'];
+                move_uploaded_file($tmpName, $path);
+
+                // Redirection vers le blog
+
+                \Http::redirect("index.php?controller=articleController&task=showAllArticlesWithPagination&msg=succeed");
+        }
 }
